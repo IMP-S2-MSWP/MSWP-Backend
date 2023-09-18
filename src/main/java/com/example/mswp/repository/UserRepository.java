a38a5e6647db5cbefd86a0576e851aebdebe6f09
Soong-moo <blueday0214@gmail.com>
2023-09-18 14:17:39 +0900

    MOB-21

--- src/main/java/com/example/mswp/repository/UserRepository.java
+++ src/main/java/com/example/mswp/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.example.mswp.repository;
 
 import com.example.mswp.entity.User;
+import com.example.mswp.mapping.UserMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findByIdAndPassword(String id, String password);
+
+    Optional<UserMapping> findAllById(String id);
 }
--- src/main/java/com/example/mswp/service/UserService.java
+++ src/main/java/com/example/mswp/service/UserService.java
@@ -2,12 +2,14 @@ package com.example.mswp.service;
 
 import com.example.mswp.dto.UserDto;
 import com.example.mswp.entity.User;
+import com.example.mswp.mapping.UserMapping;
 import com.example.mswp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -16,19 +18,27 @@ import java.util.Optional;
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
 
+    //ID, Password data로 사용자 데이터 추출
     public Optional<User> findByIdAndPassword(UserDto userDto) {
 
+        //ID 값으로 user data 추출
         Optional<User> user = userRepository.findById(userDto.getId());
-
+        
+        //user data 내 암호화된 Password와 사용자가 입력한 Password를 디코딩하여 비교
         if(user.isPresent() && passwordEncoder.matches(userDto.getPassword(), user.get().getPassword())) {
             return user;
         } else {
             return Optional.empty();
         }
 
+    }
+
+    //Password를 제외한 사용자 data 추출
+    public Optional<UserMapping> loadUser(UserDto userDto) {
+        return userRepository.findAllById(userDto.getId());
     }
 }
